ebfcc7f6418f83ee1209659ef0c801b11094ce85
abram1989 <aau89@mail.ru>
2017-02-02 20:19:02 +0300

    guessing-game:change guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,31 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    setRange(min, max) 
+		{
+        this.min=min;
+		this.max=max;	
+        
+		}
 
-    }
-
-    guess() {
-
+    guess()  {
+		if (this.min == this.max) {
+			return this.min;
+		}
+			else {
+				return Math.ceil(this.min + (this.max - this.min) / 2);
+				}
+      
     }
 
     lower() {
-
+		 this.max = this.guess();
+		
     }
 
     greater() {
+		this.min= this.guess();
+		
 
     }
 }
